59c6046f1e23c4d966cf10ea2b9bdb35b6826f28
tylerliu <tylersiqi@163.com>
2018-11-19 23:45:49 -0800

    Initial Commit

---
+++ DarkModeSwitcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DarkModeSwitcher
+//
+//  Created by Tyler on 2018/11/19.
+//  Copyright © 2018 Tyler Liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DarkModeSwitcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DarkModeSwitcher
+//
+//  Created by Tyler on 2018/11/19.
+//  Copyright © 2018 Tyler Liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
